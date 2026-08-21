71be813f093e28ae56052c8d5affb862580b9d25
Anshuman Singh <anshuman103019@gmail.com>
2026-08-22 00:30:48 +0530

    Time: 4 ms (7.57%), Space: 268.5 MB (5.2%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,12 +1,12 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        k=k%n;  //This is done because rotating an array n times gives the same array.
-        reverse(nums, 0, n-k-1);
-        reverse(nums, n-k, n-1);
-        reverse(nums, 0, n - 1);
+        k = k%n;
+        Rotate(nums, 0, n-1);
+        Rotate(nums, 0, k-1);
+        Rotate(nums, k, n-1);
     }
-     public void reverse(int[] nums, int i,int  j){
+    public static void Rotate(int[] nums, int i, int j) {
         while(i<j){
             int temp = nums[i];
             nums[i] = nums[j];
@@ -14,5 +14,5 @@ class Solution {
             i++;
             j--;
         }
-     }
+    }
 }
